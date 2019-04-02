c467e382af9d855e69f8570514554e6a9bef247b
Gaurav Raut <42151702+graut2990@users.noreply.github.com>
2019-04-02 18:02:31 +0530

    Add files via upload

---
+++ AspDotNetCoreWebApi_WindowsAuthetication/AspDotNetCoreWebApi_WindowsAuthetication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspDotNetCoreWebApi_WindowsAuthetication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
